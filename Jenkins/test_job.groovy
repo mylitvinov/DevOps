f786609aaa5ceef91c6b2a629c9566ba39d95609
mylitvinov <vinchester30@mail.ru>
2022-01-01 17:34:12 +0300

    jenkins test job

---
+++ Jenkins/test_job.groovy
@@ -0,0 +1,13 @@
+ssh root@192.168.1.201 hostname
+ssh root@192.168.1.120 hostname
+
+
+// Started by user admin
+// Running as SYSTEM
+// Building in workspace /var/lib/jenkins/workspace/test_job
+// [test_job] $ /bin/sh -xe /tmp/jenkins14811533390593994389.sh
+// + ssh root@192.168.1.201 hostname
+// ubuntuserver1
+// + ssh root@192.168.1.120 hostname
+// ubuntuserver2
+// Finished: SUCCESS
