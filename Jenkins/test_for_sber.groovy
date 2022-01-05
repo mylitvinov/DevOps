cd70f5f90f8d9d727d4d2d6bcc6b16482fe09e93
mylitvinov <vinchester30@mail.ru>
2022-01-05 19:47:23 +0300

    jenkins choice

--- Jenkins/test_for_sber.groovy
+++ Jenkins/test_for_sber.groovy
@@ -1,22 +1,58 @@
 
 pipeline {
     agent any
-  
     stages {
-        stage('First step') {
-      steps {
-        echo 'Привет, СБЕР'
+        stage('Hello, SBER') {
+          steps {
+            echo 'Привет, СБЕР'
       }
         }
-        stage('Second step') {
-      steps {
-        echo "Меня зовут $FIO "
+        stage('What is your name?') {
+          steps {
+            echo "Меня зовут $FIO "
       }
         }
-        stage('Third step') {
-      steps {
-        echo "Хочу у вас рабоать "
+        stage('I want to work for you') {
+          steps {
+            echo "Хочу у вас рабоать "
       }
+        }
+        stage('Submit') {
+           input {
+                message "Принимаем на работу?"
+                ok "Принять на работу."
+                submitter "Отказать"
+              
+            }
+            steps {
+                echo "Поздравляем, ${FIO}, вы приняты."
+            }
+        }
+        
+        stage('End') {
+           steps {
+             echo "Поздравляем, $FIO ."
+            }
+        }  
+    }
+}
+
+
+
+pipeline {
+    agent any
+    parameters { string(name: 'FIO', defaultValue: ' ', description: 'Введите ваши ФИО') }
+    stages {
+        stage('Submit') {
+            input {
+                message "Принимаем на работу?"
+                ok "Принять на работу."
+                submitter "Отказать"
+              
+            }
+            steps {
+                echo "Поздравляем, ${FIO}, вы приняты."
+            }
         }
     }
 }
