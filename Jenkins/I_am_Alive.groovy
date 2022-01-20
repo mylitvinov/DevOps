8892263dfdeeb6e50442450b709c535d16956341
mylitvinov <vinchester30@mail.ru>
2022-01-20 20:36:26 +0300

    jenkins

---
+++ Jenkins/I_am_Alive.groovy
@@ -0,0 +1,11 @@
+pipeline {
+       agent any
+
+       stages {
+           stage('Connection test') {
+               steps { script {          
+                   print  "I am alive!"                
+               }}
+           }
+       }    
+   }
