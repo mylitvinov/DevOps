4134d30025dab9a25e22e30a7f7d0b1b7d0143c2
Maxim Litvinov <88111350+mylitvinov@users.noreply.github.com>
2022-02-26 11:27:46 +0300

    Create SB_TEST2.groovy

---
+++ Jenkins/sripts_groovy/SB_TEST2.groovy
@@ -0,0 +1,61 @@
+pipeline {
+    agent {
+        node {
+
+            label 'Linux_Default'    
+        }
+    }
+    parameters {
+      string(name: 'MY_NAME', defaultValue: '', description: 'ФИО')
+      string(name: 'E_MAIL', defaultValue: '', description: 'Введите вашу почту')
+
+    }
+
+    stages {
+        stage('Hi') {
+            steps {
+                echo "Привет Компания"
+            }
+        }
+        stage('My name') {
+            steps {
+                echo "Меня зовут ${params.MY_NAME}"
+            }
+        }
+        stage('Work') {
+            steps {
+                echo "Хочу у вас работать"
+            }
+        }
+        stage('Approve') {
+			steps {
+				script {
+						 def ANSWER = input(
+								message: 'Принять?',
+								parameters: [
+										[$class: 'ChoiceParameterDefinition',
+										 choices: ['Отказать','Принять на работу'].join('\n'),
+										 name: '',
+										 description: 'Выберите результат']
+								])
+
+						echo "The answer is: ${ANSWER}"
+
+						if( "${ANSWER}" == "Принять на работу"){
+							
+						} else {
+							  autoCancelled = true
+							  error('Отказ')
+						}
+				}
+			}
+		}
+      //   stage('Mail') {
+      //       steps {
+			// 	emailext body: "${params.MY_NAME} принят в компанию ",
+			// 		subject: "Результат собеседования",
+			// 		to: "${params.E_MAIL}"
+			// }
+      //   }
+    }		
+}
